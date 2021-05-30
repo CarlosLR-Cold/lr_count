19fea9fef2fdeee6f33221e63ff1b44578618aa2
Carlos Daniel <20032000dani@gmail.com>
2021-05-30 11:58:05 +0200

    Structure organization
    Organized the files to start code

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'CarlosLR-Count'
+
+author 'CarlosLR'
+
+version '5.0'
+
+client_scripts {
+	'config.lua',
+	'client/classes/BaseController.lua',
+	'client/main.lua'
+}
+server_scripts {
+	'config.lua',
+	'server/classes/BaseController.lua',
+	'server/main.lua'
+}
+
+
---
+++ server/classes/BaseController.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'CarlosLR-Count'
+
+author 'CarlosLR'
+
+version '5.0'
+
+client_scripts {
+	'config/config.lua',
+	'client.lua'
+}
+server_scripts {
+	'config/config.lua',
+	'server.lua'
+}
+
+
